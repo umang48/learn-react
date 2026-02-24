cf8dc8bd8c85fc8bff849b1e4f4f1a6ac587cb51
umang <umangitdeveloper@gmail.com>
2026-02-24 21:58:09 +0530

    learn javascript array methods

--- learn-js-fundamentals.js
+++ learn-js-fundamentals.js
@@ -3,3 +3,16 @@ console.log(yourname);
 
 const greet = (myname) => { console.log("Hello ", myname)};
 console.log(greet("Full Stack Developer"));
+
+// array methods
+
+const numbers = [1, 2, 3, 4, 5];
+
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);   
+
+const evennumbers = numbers.filter(num => num % 2 === 0);
+console.log(evennumbers);
+
+const largerthanthree = numbers.find(num => num > 3);
+console.log(largerthanthree);
