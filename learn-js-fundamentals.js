192aff39afff7130586d29f7c12600f05a393cfe
umang <umangitdeveloper@gmail.com>
2026-02-24 22:47:10 +0530

    spread operator

--- learn-js-fundamentals.js
+++ learn-js-fundamentals.js
@@ -24,3 +24,12 @@ const user = {name:"Umang", age:32, city:"Unjha"};
 const {name, age, city} = user;
 console.log(age)
 
+// spread operator
+
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1,4,5,6];
+console.log(arr2);
+
+const obj1 = {a:1,b:2};
+const obj2 = {...obj1, c:3};
+console.log(obj2);
